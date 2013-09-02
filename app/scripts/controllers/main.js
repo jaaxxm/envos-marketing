2311e90ce986381b54c4d055d29b6ec1ea60071a
jaaxxm <jaaxxm@gmail.com>
2013-09-03 00:00:34 +0300

    1st commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('envosMarketingApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
