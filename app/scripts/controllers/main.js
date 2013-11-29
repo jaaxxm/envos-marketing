2837a26c54e2cca5808e6884c61c743c8bbf0b0e
jaaxxm <jaaxxm@gmail.com>
2013-11-29 20:29:11 +0200

    - new serie added - products list moved to collapsed blocks - detailed page tabs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,25 @@
 'use strict';
 
 angular.module('envosMarketingApp')
-  .controller('MainCtrl', function ($scope, $modal, prodBench) {
+  .controller('MainCtrl', function ($scope, prodBench) {
 
+	  $scope.isCollapsed = [true, true, true];
+	  
+	  $scope.openSerie = function(e)
+	  {
+	  	if($scope.isCollapsed[e] = false) {
+	  		$scope.isCollapsed[e] = true;
+	  	} else {	  		
+		    for(var i=0;i<$scope.isCollapsed.length;i++)
+		      $scope.isCollapsed[i]=true;
+		    $scope.isCollapsed[e] = !$scope.isCollapsed[e];
+	  	}
+	  }
+
+	  $scope.hideSerie = function(e)
+	  {	   
+	    $scope.isCollapsed[e] = true;
+	  }
 
 		prodBench.async().then(function(d) {
 	    $scope.Bench = d;
